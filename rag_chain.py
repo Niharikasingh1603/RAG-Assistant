2c1b7c401a5af5dee2d27ae411d5ad9d51499454
Niharika Singh <121479871+Niharikasingh1603@users.noreply.github.com>
2025-09-20 11:12:54 +0530

    Update rag_chain.py

--- rag_chain.py
+++ rag_chain.py
@@ -1,7 +1,7 @@
 import os
 from langchain_community.document_loaders import PyPDFLoader, TextLoader, CSVLoader, UnstructuredWordDocumentLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_genai import ( GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI, )
+from langchain_google_genai import ( ChatGoogleGenerativeAI )
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
@@ -28,16 +28,28 @@ def load_docs(file_path: str):
         raise ValueError("Unsupported file type")
     return loader.load()
 
-def create_vectorstore(docs):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+def create_vectorstore(docs, size_kb, size_mb):
+    if size_kb <= 200:
+        splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
+    elif size_kb > 200 and size_mb <= 2:
+        splitter = RecursiveCharacterTextSplitter(chunk_size=850, chunk_overlap=150)
+    elif size_mb > 2 and size_mb <= 10:
+        splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+    else: # size_mb > 10
+        splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=250)
     chunks = splitter.split_documents(docs)
-    
-    #embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     return FAISS.from_documents(chunks, embeddings)
 
-def get_rag_chain(vectordb):
-    retriever = vectordb.as_retriever(search_type="similarity", search_kwargs={"k":4})
+def get_rag_chain(vectordb, size_kb, size_mb):
+    if size_kb <= 200:
+        retriever = vectordb.as_retriever(search_type="similarity", search_kwargs={"k":6})
+    elif size_kb > 200 and size_mb <= 2:
+        retriever = vectordb.as_retriever(search_type="mmr", search_kwargs={"k":10, "fetch_k":20, "lambda_mult":0.5})
+    elif size_mb > 2 and size_mb <= 10:
+        retriever = vectordb.as_retriever(search_type="mmr", search_kwargs={"k":13, "fetch_k":25, "lambda_mult":0.5})
+    else: # size_mb > 10
+        retriever = vectordb.as_retriever(search_type="mmr+rerank", search_kwargs={"k":16, "fetch_k":30, "lambda_mult":0.3, "rerank_top_k":6})
     prompt = PromptTemplate(
         input_variables=["context", "question"],
         template = '''
@@ -53,11 +65,6 @@ def get_rag_chain(vectordb):
         '''
     )
     llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.3)
-
-    '''parallel_chain = RunnableParallel(
-        context = retriever | RunnableLambda(lambda retrieved_docs: "\n\n".join(doc.page_content for doc in retrieved_docs)),
-        question = RunnablePassthrough()
-    )'''
 
     parser = StrOutputParser()
 
@@ -77,7 +84,7 @@ def get_rag_chain(vectordb):
         context_text = inputs["context"]["context_text"]
         docs = inputs["context"]["docs"]
         formatted_prompt = prompt.format(context=context_text, question=inputs["question"])
-        answer = llm.invoke(formatted_prompt)
-        return {"answer": answer, "retrieved_docs": docs}
+        for chunk in llm.stream(formatted_prompt):
+            yield {"answer": chunk, "retrieved_docs": docs}
 
     return parallel_chain | RunnableLambda(process_output)
